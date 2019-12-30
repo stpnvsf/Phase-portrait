512eac8efca4d529164e6c0ba7e266b22f244a62
stpnvsf <stpnvsfj@gmail.com>
2019-12-31 02:19:48 +0400

    1111

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(12, 12);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(600, 600);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            this.AnT.Load += new System.EventHandler(this.AnT_Load);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 623);
+            this.Controls.Add(this.AnT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using Tao.Platform.Windows;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        List<double> coordinates = new List<double>();
+        double[,] mas = new double[1600,3];
+        public Form1()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Glut.glutInit();//библиотека и методы Glut работают с ОС и конкретной аппаратурой
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);//для отображения кадра 
+            //будет использоваться двойная буферизация, убирает мерцание монитора
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluOrtho2D(-2.0, 2.0, -2.0, 2.0);
+            //нужна еще одна объектно-видовая матрица для переноса
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+            Print();
+
+            timer1.Start();
+
+        }
+        public void Print()
+        {
+            mas = new double[1600, 3];
+            Random rand = new Random();
+            int k = 0;
+            for (double i = -2.0; i < 2.0; i += 0.1)
+            {
+                for (double j = -2.0; j < 2.0; j += 0.1)
+                {
+                    mas[k, 0] = i;
+                    mas[k, 1] = j;
+                    mas[k, 2] = rand.NextDouble();
+                    k++;
+                }
+            }
+        }
+        private void Drawing()
+        {
+            //Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+            Gl.glColor3f(0.0f, 1.0f, 1.0f);
+            Gl.glBegin(Gl.GL_POINTS);
+            for (int i = 0; i < 1600; i++)
+            {
+                Gl.glVertex2f((float)mas[i, 0], (float)mas[i, 1]);
+            }
+
+            Gl.glEnd();
+            AnT.Invalidate();
+
+        }
+
+        private void f()
+        {
+            /*for (int i = 0; i < 1600; i++)//cедло
+            {
+                mas[i, 0] += (- (mas[i, 0] * mas[i, 0]) + mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+                mas[i, 1] += (mas[i, 0] * mas[i, 0] - 2.0 * mas[i, 0] *mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+            }*/
+            for (int i = 0; i < 1600; i++)//cедло
+            {
+                mas[i, 0] += (0.0 * mas[i, 0] + 1.0 * mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+                mas[i, 1] += (2.0 * (mas[i, 0] - mas[i, 0] * mas[i, 0] * mas[i, 0]) - 1.0 * mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+            }
+            //for (int i = 0; i < 1600; i++)//неустойчивый фокус
+            //{
+            //    mas[i, 0] += (3.0 * mas[i, 0] - 4.0 * mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+            //    mas[i, 1] += (2.0 * mas[i, 0] - 1.0 * mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+            //}
+
+            /*for (int i = 0; i < 1600; i++)//cедло
+            {
+                mas[i, 0] += (3.0 * mas[i, 0] - 4.0 * mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+                mas[i, 1] += (-3.0 * mas[i, 0] - 3.0 * mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+            }*/
+
+            /*for (int i = 0; i < 1600; i++)//неустойчивый узел
+            {
+                mas[i, 0] += (-1.0 * mas[i, 0] - 1.0 * mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+                mas[i, 1] += (1.0 * mas[i, 0] - 3.0 * mas[i, 1]) * timer1.Interval / 1000.0 * mas[i, 2];
+            }*/
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Drawing();
+            f();
+        }
+        private void AnT_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
